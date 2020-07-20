56b2de06e5f11bde5f03df46a3361f01d30c7cc9
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-20 23:15:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,12 @@
-def square_array(array)
-  squared = []
-  array.each { |element| squared << element ** 2 }
-  squared
+
+
+
+def square_array(array) 
+  new_array = []
+  array.each do |num|
+    bew_array.push(num ^ 2)
+  end
+  new_array
 end
+
+square_array(1, 2, 3, 4, 5)
