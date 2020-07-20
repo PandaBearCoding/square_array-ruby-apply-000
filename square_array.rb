cb92bacf7d714ef31b160dcc7162834941011b3e
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-20 23:20:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,16 +4,5 @@ def square_array(array)
   squared
 end
 
-square_array(1, 2, 3, 4, 5)
 
 
-
-def square_array(array) 
-  new_array = []
-  array.each do |num|
-    bew_array.push(|num| ** 2)
-  end
-  new_array
-end
-
-square_array(1, 2, 3, 4, 5)
